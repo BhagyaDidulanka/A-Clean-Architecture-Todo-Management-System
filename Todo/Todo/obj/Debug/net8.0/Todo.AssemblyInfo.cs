a074298e05781aea42871807fb4e635dc89e9f1b
BhagyaDidulanka <didulankabhagya@gmail.com>
2025-04-10 10:07:50 +0530

    Dto class

--- Todo/Todo/obj/Debug/net8.0/Todo.AssemblyInfo.cs
+++ Todo/Todo/obj/Debug/net8.0/Todo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Todo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca7060c2e3c3c92fe19c59a46031ee560f8a0578")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fc8b0a15f9edc121c10292629c618c2cb36b578")]
 [assembly: System.Reflection.AssemblyProductAttribute("Todo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Todo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
